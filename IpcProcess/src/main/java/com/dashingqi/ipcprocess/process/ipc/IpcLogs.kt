75976c0ec0d578f3ebe3b75f00ec4f0ae5fd6a5e
zhangqi61 <zhangqi61@baidu.com>
2023-12-04 16:14:23 +0800

    【ipc】【update】

--- IpcProcess/src/main/java/com/dashingqi/ipcprocess/process/ipc/IpcLogs.kt
+++ IpcProcess/src/main/java/com/dashingqi/ipcprocess/process/ipc/IpcLogs.kt
@@ -1,9 +1,30 @@
 package com.dashingqi.ipcprocess.process.ipc
 
 import android.util.Log
+import com.dashingqi.ipcprocess.BuildConfig
 
 
 const val IPC_TAG = "IcpDelegateTag"
+
+/** debug 开关*/
+internal const val DEBUG = BuildConfig.IpcDebug
+
+/**
+ * 打印字符串 Debug等级
+ * @receiver String?
+ */
 fun String?.logD() {
-    Log.d(IPC_TAG, this ?: "this is error")
+    if (DEBUG) {
+        Log.d(IPC_TAG, this ?: "this is error")
+    }
+}
+
+/**
+ * 打印Throwable错误信息
+ * @receiver Throwable
+ */
+fun Throwable.printStack() {
+    if (DEBUG) {
+        this.printStackTrace()
+    }
 }
